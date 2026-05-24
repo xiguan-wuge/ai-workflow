1c0dfd220008b81e113bba577db13c36f070ad83
xiguan wuge <1584077483@qq.com>
2026-05-24 16:55:29 +0800

    feat(playground): 引入elementPlus 组件测试用例

---
+++ playground/src/components/test1.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-checkbox v-model="checked1" disabled>Disabled</el-checkbox>
+  <el-checkbox v-model="checked2">Not disabled</el-checkbox>
+  <hr>
+  <el-color-picker-panel v-model="color" />
+  <hr>
+  <div class="demo-color-block">
+    <span class="demonstration">With default value</span>
+    <el-color-picker v-model="color1" />
+  </div>
+  <div class="demo-color-block">
+    <span class="demonstration">With no default value</span>
+    <el-color-picker v-model="color2" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ElColorPicker, ElColorPickerPanel, ElCheckbox } from 'element-plus'
+import { ref } from 'vue'
+
+const checked1 = ref(false)
+const checked2 = ref(true)
+const color = ref('#409EFF')
+const color1 = ref('#409EFF')
+const color2 = ref()
+
+</script>
